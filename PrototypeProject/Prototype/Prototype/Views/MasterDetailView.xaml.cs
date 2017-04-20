48c6c1ac30ea1ff3e90037cc6770f9f0e90e6ccf
DESKTOP\duck2412 <michael.moeller84@gmail.com>
2017-04-20 09:43:09 +0200

    XP-43: MasterDetail er nu startsiden - virker

---
+++ PrototypeProject/Prototype/Prototype/Views/MasterDetailView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Prototype.Views
+{
+    public partial class MasterDetailView : MasterDetailPage
+    {
+        public MasterDetailView()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new FrontPageView());
+        }
+
+        private void FrontPageAction(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
