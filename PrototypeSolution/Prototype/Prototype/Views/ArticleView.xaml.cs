4c8ebfd225c43dae99f3b5173b7614368a6033b6
Søren Valentin Hansen <svseboks@gmail.com>
2017-04-24 10:51:18 +0200

    XP-19 added ArticleView dummy and navigation testcode from AllArticlesView.

---
+++ PrototypeSolution/Prototype/Prototype/Views/ArticleView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prototype.ModelControllers;
+using Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ArticleView : ContentPage
+	{
+        private StateController StateController;
+        private Article Article;
+		public ArticleView (StateController stateController, Article article)
+		{
+			InitializeComponent ();
+            this.StateController = stateController;
+            this.Article = article;
+            BindingContext = Article;
+		}
+	}
+}
