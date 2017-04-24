8d457bb755fb58c4d795088a065affc38d4fcf3c
DESKTOP\duck2412 <michael.moeller84@gmail.com>
2017-04-24 14:25:12 +0200

    XP-2: INotifyPropertyChanged implementeret i article.cs

--- PrototypeSolution/Prototype/Prototype/Models/Article.cs
+++ PrototypeSolution/Prototype/Prototype/Models/Article.cs
@@ -1,29 +1,191 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Prototype.Models
 {
-    public class Article
+    public class Article : INotifyPropertyChanged
     {
-        public string Title { get; set; }
-        public string ContentURL { get; set; }
-        public string BodyText { get; set; }
-        public string HomeSectionName { get; set; }
-        public Boolean Locked { get; set; }
-        public DateTime PublishedDate { get; set; }
-        public string PublishInfo { get; set; }
-        public int HomeSectionId { get; set; }
-        public int Id { get; set; }
-        public string ImageCaption { get; set; }
-        public string Teaser { get; set; }
-        public string ImageBigURL { get; set; }
-        public string ImageSmallURL { get; set; }
-        public string ImageThumbURL { get; set; }
-
-        public Article()
+        string title;
+        public string Title
         {
+            get { return title; }
+            set
+            {
+                if(title == value) { return; }
+                title = value;
+                notify("Title");
+            }
+        }
 
+        string contentURL;
+        public string ContentURL
+        {
+            get { return contentURL; }
+            set
+            {
+                if (contentURL == value) { return; }
+                contentURL = value;
+                notify("ContentURL");
+            }
+        }
+
+        string bodyText;
+        public string BodyText
+        {
+            get { return bodyText; }
+            set
+            {
+                if (bodyText == value) { return; }
+                bodyText = value;
+                notify("BodyText");
+            }
+        }
+
+        string homeSectionName;
+        public string HomeSectionName
+        {
+            get { return homeSectionName; }
+            set
+            {
+                if (homeSectionName == value) { return; }
+                homeSectionName = value;
+                notify("HomeSectionName");
+            }
+        }
+
+        Boolean locked;
+        public Boolean Locked
+        {
+            get { return locked; }
+            set
+            {
+                if (locked == value) { return; }
+                locked = value;
+                notify("Locked");
+            }
+        }
+
+        DateTime publishedDate;
+        public DateTime PublishedDate
+        {
+            get { return publishedDate; }
+            set
+            {
+                if (publishedDate == value) { return; }
+                publishedDate = value;
+                notify("PublishedDate");
+            }
+        }
+
+        string publishInfo;
+        public string PublishInfo
+        {
+            get { return publishInfo; }
+            set
+            {
+                if (publishInfo == value) { return; }
+                publishInfo = value;
+                notify("PublishInfo");
+            }
+        }
+
+        int homeSectionId;
+        public int HomeSectionId
+        {
+            get { return homeSectionId; }
+            set
+            {
+                if (homeSectionId == value) { return; }
+                homeSectionId = value;
+                notify("HomeSectionId");
+            }
+        }
+
+        int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id == value) { return; }
+                id = value;
+                notify("Id");
+            }
+        }
+
+        string imageCaption;
+        public string ImageCaption
+        {
+            get { return imageCaption; }
+            set
+            {
+                if (imageCaption == value) { return; }
+                imageCaption = value;
+                notify("ImageCaption");
+            }
+        }
+
+        string teaser;
+        public string Teaser
+        {
+            get { return teaser; }
+            set
+            {
+                if (teaser == value) { return; }
+                teaser = value;
+                notify("Teaser");
+            }
+        }
+
+        string imageBigURL;
+        public string ImageBigURL
+        {
+            get { return imageBigURL; }
+            set
+            {
+                if (imageBigURL == value) { return; }
+                imageBigURL = value;
+                notify("ImageBigURL");
+            }
+        }
+
+        string imageSmallURL;
+        public string ImageSmallURL
+        {
+            get { return imageSmallURL; }
+            set
+            {
+                if (imageSmallURL == value) { return; }
+                imageSmallURL = value;
+                notify("ImageSmallURL");
+            }
+        }
+
+        string imageThumbURL;
+        public string ImageThumbURL
+        {
+            get { return imageThumbURL; }
+            set
+            {
+                if (imageThumbURL == value) { return; }
+                imageThumbURL = value;
+                notify("ImageThumbURL");
+            }
+        }
+
+        public Article() { }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void notify(string propName)
+        {
+            if(this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
         }
     }
 }
