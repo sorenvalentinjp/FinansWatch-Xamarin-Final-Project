549b91f12fa88d7ca93a3a34f6134d86f2bd6ff4
Søren Valentin Hansen <svseboks@gmail.com>
2017-04-24 14:09:32 +0200

    XP-19 Created CustomRenderer HtmlFormattedLabel and added it to ArticleView

---
+++ PrototypeSolution/Prototype/Prototype.iOS/CustomRenderers/HtmlFormattedLabelRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using Prototype.iOS.CustomRenderers;
+using Prototype.Views.CustomRenderers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HtmlFormattedLabel), typeof(HtmlFormattedLabelRenderer))]
+namespace Prototype.iOS.CustomRenderers
+{
+    class HtmlFormattedLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var view = (HtmlFormattedLabel)Element;
+            if (view == null) return;
+
+            var attr = new NSAttributedStringDocumentAttributes();
+            var nsError = new NSError();
+            attr.DocumentType = NSDocumentType.HTML;
+
+            Control.AttributedText = new NSAttributedString(view.Text, attr, ref nsError);
+        }
+    }
+}
---
+++ PrototypeSolution/Prototype/Prototype/Views/CustomRenderers/HtmlFormattedLabel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Prototype.Views.CustomRenderers
+{
+    class HtmlFormattedLabel : Label
+    {
+    }
+}
