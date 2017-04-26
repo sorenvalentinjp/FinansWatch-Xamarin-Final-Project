380f0ab86eee11fe94f106f8b3824da0b16ce80d
Søren Valentin Hansen <svseboks@gmail.com>
2017-04-26 16:31:56 +0200

    started work on relatedArticle listviews. Added mediumArticleTemplateSelector

---
+++ PrototypeSolution/Prototype/Prototype/Views/MediumArticleTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Prototype.Views
+{
+    /// <summary>
+    /// This class defines which template a given article should be using in related articles listviews and similar medium sized cell listviews..
+    /// </summary>
+    public class MediumArticleTemplateSelector : DataTemplateSelector
+    {
+
+        public DataTemplate ArticleMediumTemplate { get; set; }
+        public DataTemplate ArticleMediumNoImageTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            Article article = (Article)item;
+
+            if (article.ImageSmallURL == "")
+                return ArticleMediumNoImageTemplate;
+            else
+                return ArticleMediumTemplate;
+        }
+    }
+}
