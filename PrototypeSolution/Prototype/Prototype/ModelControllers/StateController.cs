1fac712833ad2482df67b1f7d73c6027db730a04
DESKTOP\duck2412 <michael.moeller84@gmail.com>
2017-04-28 12:12:56 +0200

    SavedArticlesview har nu lidt funktionalitet, hvor artikler tilføjet i onappearing hvorefter de kan fjernes ved et tab

--- PrototypeSolution/Prototype/Prototype/ModelControllers/StateController.cs
+++ PrototypeSolution/Prototype/Prototype/ModelControllers/StateController.cs
@@ -1,6 +1,7 @@
 ﻿using Prototype.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,20 +11,22 @@ namespace Prototype.ModelControllers
     public class StateController
     {
         private ArticleController articleController;
+        public ObservableCollection<Article> SavedArticles { get; set; }
 
         public StateController()
         {
-            articleController = new ArticleController();
+            this.articleController = new ArticleController();
+            this.SavedArticles = new ObservableCollection<Article>();
         }
 
         public Task<List<Article>> getFrontPageArticles()
         {
-            return articleController.getFrontPageArticles();
+            return this.articleController.getFrontPageArticles();
         }
 
         public Task<Article> getArticleDetails(Article article)
         {
-            return articleController.getArticleDetails(article);
+            return this.articleController.getArticleDetails(article);
         }
     }
 
