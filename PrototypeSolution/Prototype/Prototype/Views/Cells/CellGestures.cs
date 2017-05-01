dcd8015cde08fb9e7aa42f9be4490d3352047a7f
DESKTOP\duck2412 <michael.moeller84@gmail.com>
2017-05-01 16:26:20 +0200

    Stor refaktorering. Cellerne har nu fået deres egne klasser. Logik for gestures er flyttet ud i disse klasser.

---
+++ PrototypeSolution/Prototype/Prototype/Views/Cells/CellGestures.cs
@@ -0,0 +1,31 @@
+﻿using Prototype.ModelControllers;
+using Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Prototype.Views.Cells
+{
+    public static class CellGestures
+    {
+        public static async void TappedGesture(ContentPage page, StateController stateController, Article article)
+        {
+            await page.Navigation.PushModalAsync(new NavigationPage(new ArticleView(stateController, article)), true);
+        }
+
+        public static async void LongPressingGesture(ContentPage page, StateController stateController, Article article)
+        {
+            if (stateController.SavedArticles.Contains(article))
+            {
+                stateController.SavedArticles.Remove(article);
+                await page.DisplayAlert("", "Artiklen er fjernet fra læselisten.", "OK");
+            }
+            else
+            {
+                stateController.SavedArticles.Add(article);
+                await page.DisplayAlert("", "Artiklen er gemt i læselisten.", "OK");
+            }
+        }
+    }
+}
