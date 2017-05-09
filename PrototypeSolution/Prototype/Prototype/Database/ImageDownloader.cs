53d58e9b07fdf8c2df05fb5491764330058bd145
Søren Valentin Hansen <svseboks@gmail.com>
2017-05-09 12:14:23 +0200

    XP-155 Added ImageDownloader class. Not in use yet

---
+++ PrototypeSolution/Prototype/Prototype/Database/ImageDownloader.cs
@@ -0,0 +1,34 @@
+﻿using Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prototype.Database
+{
+    public class ImageDownloader
+    {
+        private readonly HttpClient _client;
+        public ImageDownloader()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<ImageSource> DownloadImage(string url)
+        {
+            return await Task.Run(async () =>
+            {
+                //Download the image async
+                var uri = new Uri(url);
+                var imageAsBytes = await _client.GetByteArrayAsync(uri); // get the downloaded data
+
+                return ImageSource.FromStream(() => new MemoryStream(imageAsBytes)); // Create imagesource with downloaded data
+            });
+
+        }
+    }
+}
