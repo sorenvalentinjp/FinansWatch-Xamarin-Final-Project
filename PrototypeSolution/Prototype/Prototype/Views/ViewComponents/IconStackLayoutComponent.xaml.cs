350ce1189e0f9d50cb262103c604293197c5e7ba
Søren Valentin Hansen <svseboks@gmail.com>
2017-05-11 14:13:36 +0200

    XP-123 Renamed components namespace to viewComponents because of gitignore.

--- PrototypeSolution/Prototype/Prototype/Views/ViewComponents/IconStackLayoutComponent.xaml.cs
+++ PrototypeSolution/Prototype/Prototype/Views/ViewComponents/IconStackLayoutComponent.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Prototype.Views.Components
+namespace Prototype.Views.ViewComponents
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class IconStackLayoutComponent : StackLayout
