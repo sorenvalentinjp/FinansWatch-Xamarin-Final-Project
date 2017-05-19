84287e6fda4741691dfb661a4d10716c62498180
Søren Valentin Hansen <svseboks@gmail.com>
2017-05-19 09:34:29 +0200

    XP-186 Added subscriberTokenTest and empty ArticleControllerTest class

---
+++ PrototypeSolution/Prototype.UITest/UnitTests/ModelControllers/ArticleControllerTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Prototype.ModelControllers;
+using Prototype.Models;
+
+namespace Prototype.UITest.UnitTests.ModelControllers
+{
+    public class ArticleControllerTest
+    {
+        ArticleController _articleController;
+        [SetUp]
+        public void Setup()
+        {
+            _articleController = new ArticleController();
+        }
+
+        [Test]
+        public void Bucket1IsReadyEventShouldBeFired()
+        {
+            //Prepare
+
+            //Assert
+
+        }
+        [Test]
+        public void Bucket2IsReadyEventShouldBeFired()
+        {
+            //Prepare
+
+            //Assert
+
+        }
+        [Test]
+        public void SavedArticlesChangedEventShouldBeFired()
+        {
+            //Prepare
+
+            //Assert
+
+        }
+        [Test]
+        public void IsRefreshingFrontPageEventShouldBeFired()
+        {
+            //Prepare
+
+            //Assert
+
+        }
+        [Test]
+        public void IsRefreshingLatestArticlesShouldBeFired()
+        {
+            //Prepare
+
+            //Assert
+
+        }
+    }
+}
---
+++ PrototypeSolution/Prototype.UITest/UnitTests/Models/SubscriberTokenTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.UITest.UnitTests.Models
+{
+    class SubscriberTokenTest
+    {
+        //This class does not contain any logic so there is no reason to test it.
+    }
+}
