177cb413bb32d6b7c1816b6cdab144fa1b130bad
DESKTOP\duck2412 <michael.moeller84@gmail.com>
2017-05-22 10:15:43 +0200

    XP-212: StateControllerTest done. No tests because all methods just passes the calls to the coresponding ArticleController-method

---
+++ PrototypeSolution/Prototype.UITest/UnitTests/ModelControllers/StateControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.UITest.UnitTests.ModelControllers
+{
+    /// <summary>
+    /// StateController.cs only contains methods that passes the call to ArticleController.cs.
+    /// Therefore no testing is done currently.
+    /// </summary>
+    [TestFixture]
+    public class StateControllerTest
+    {
+    }
+}
