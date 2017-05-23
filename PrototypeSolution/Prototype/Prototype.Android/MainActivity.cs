9f079c5c6978c85bb54bfb6022d65537e619ec20
Søren Valentin Hansen <svseboks@gmail.com>
2017-05-23 10:56:18 +0200

    XP-246 Added splashscreen to android

--- PrototypeSolution/Prototype/Prototype.Android/MainActivity.cs
+++ PrototypeSolution/Prototype/Prototype.Android/MainActivity.cs
@@ -10,15 +10,17 @@ using FFImageLoading.Forms.Droid;
 
 namespace Prototype.Droid
 {
-	[Activity (Label = "Prototype", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "Prototype", Icon = "@drawable/icon", Theme="@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
-			ToolbarResource = Resource.Layout.Toolbar; 
+			ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate (bundle);
+		    base.SetTheme(Resource.Style.MainTheme);
+
+            base.OnCreate (bundle);
 
             CachedImageRenderer.Init();
 
