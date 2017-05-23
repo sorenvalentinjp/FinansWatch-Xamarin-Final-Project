02ea7a188b0ade3e3da61c49cbda93518b46fdb6
DESKTOP\duck2412 <michael.moeller84@gmail.com>
2017-05-23 09:39:00 +0200

    XP-220: SavedArticlesViewModelTest done. Also added comments to the base class.

---
+++ PrototypeSolution/Prototype.UITest/UnitTests/ViewModels/SavedArticlesViewModelTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Prototype.ModelControllers;
+using Prototype.Models;
+using Prototype.UITest.Mocks;
+using Prototype.UITest.UnitTests.Helpers;
+using Prototype.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prototype.UITest.UnitTests.ViewModels
+{
+    [TestFixture]
+    public class SavedArticlesViewModelTest
+    {
+        StateController _stateController;
+        SavedArticlesViewModel _vm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stateController = new StateController();
+            _vm = new SavedArticlesViewModel(_stateController);
+        }
+
+        [Test]
+        public void SavedArticlesShouldRaisePropertyChangedEvent()
+        {
+            PropertyChangedAsserter.AssertPropertyChanged(_vm, (x) => x.SavedArticles = new ObservableCollection<ArticleViewModel>(), "SavedArticles");
+        }
+
+        [Test]
+        public void DataTemplateShouldRaisePropertyChangedEvent()
+        {
+            PropertyChangedAsserter.AssertPropertyChanged(_vm, (x) => x.DataTemplate = new DataTemplate(), "DataTemplate");
+        }
+
+        //No need to test this as it is using a method in the ArticleViewModel, that we already tested
+        //Also, we cant really do it, because we cant use Moq to simulate the invoke of the event.
+        //[Test]
+        //public void LoginEventShouldCalculateIfTheArticlesShouldBeLocked()
+        //{
+            
+        //}
+
+        //This test also confirms, that GetSavedArticles is working properly
+        [Test]
+        public void SavedArticles_CollectionChangedShouldBeUpdatedWhenTheCollectionIsChanged()
+        {
+            //Arrange
+            Assert.True(_vm.SavedArticles.Count == 0);
+            Article articleToAdd = new Article();
+
+            //Act
+            _stateController.ArticleController.SavedArticles.Add(articleToAdd);
+
+            //Assert
+            Assert.True(_vm.SavedArticles.Count == 1);
+        }
+    }
+}
--- PrototypeSolution/Prototype/Prototype/ViewModels/SavedArticlesViewModel.cs
+++ PrototypeSolution/Prototype/Prototype/ViewModels/SavedArticlesViewModel.cs
@@ -43,33 +43,46 @@ namespace Prototype.ViewModels
         {
             this._stateController = stateController;
             this.SavedArticles = GetSavedArticles(); //to populate the list when the view loads
-            //because our statecontroller's SavedArticles property contains Article objects, we cant reference that collection directly (we use ArticleViewModels here). Therefore we mus t create this event listener to get notified when the collection changes
             _stateController.ArticleController.SavedArticles.CollectionChanged += SavedArticles_CollectionChanged;
             this.DataTemplate = new SavedArticlesTemplateSelector(_stateController);
             this._stateController.LoginController.LoginEventSucceeded += LoginEvent;
         }
 
-        //Subscribed Event
-        //If the user just logged in, recalculate if the article should display as locked
+        /// <summary>
+        /// If the user just logged in, recalculate if the article should display as locked
+        /// </summary>
+        /// <param name="subscriber"></param>
         private void LoginEvent(Subscriber subscriber)
         {
-                foreach (var articleViewModel in SavedArticles)
-                {
-                    articleViewModel.CalculateIfArticleShouldBeLocked();
-                }
+            foreach (var articleViewModel in SavedArticles)
+            {
+                articleViewModel.CalculateIfArticleShouldBeLocked();
+            }
         }
 
+        /// <summary>
+        /// Every time the ArticleController's SavedArticles change, this event is invoked.
+        /// This way we update the list stored in SavedArticlesViewModel.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SavedArticles_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
             this.SavedArticles = GetSavedArticles();
         }
 
-        //Helper method to get the statecontroller's SavedArticles
+        /// <summary>
+        /// Gets the saved articles stored in the ArticleController and creates an ArticleViewModel for each one.
+        /// </summary>
+        /// <returns></returns>
         private ObservableCollection<ArticleViewModel> GetSavedArticles()
         {
             ObservableCollection<ArticleViewModel> toReturn = new ObservableCollection<ArticleViewModel>();
             foreach (var article in _stateController.ArticleController.SavedArticles)
+            {
                 toReturn.Add(new ArticleViewModel(_stateController, article));
+            }
+                
             return toReturn;
         }
 
